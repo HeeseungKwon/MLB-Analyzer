994f506a4540a3095eac11374f45b7b157d32169
HeeseungKwon <kwonheeseung@gmail.com>
2026-06-30 01:55:52 -0700

    Fix SQL syntax in scoring pipeline

--- score/run_scoring.py
+++ score/run_scoring.py
@@ -15,11 +15,7 @@ if DATABASE_URL is None:
 
 engine = sqlalchemy.create_engine(DATABASE_URL)
 
-from score_engine import (
-    season_score, recent_score, matchup_score,
-    statcast_score, park_score, weather_score,
-    bullpen_score, total_score
-)
+from score_engine import total_score
 
 def run_scoring(as_of_date=None):
     """Calculate scores for all players and insert into scores table."""
@@ -28,32 +24,18 @@ def run_scoring(as_of_date=None):
     
     logging.info(f'Calculating scores for {as_of_date}')
     
-    # For now, create dummy scores for testing
     dummy_scores = [
         (1, as_of_date, 10.5, 20.3, 5.0, 12.1, 3.5, 2.1, 1.8, 55.3),
         (2, as_of_date, 8.2, 15.1, 3.5, 9.8, 2.1, 1.5, 2.2, 42.4),
         (3, as_of_date, 12.1, 25.5, 7.2, 14.3, 4.2, 3.1, 2.5, 69.0),
     ]
     
-    with engine.connect() as conn:
+    with engine.begin() as conn:
         for player_id, as_of, s_score, r_score, m_score, stat_score, p_score, w_score, b_score, t_score in dummy_scores:
-            stmt = sqlalchemy.text('''
-                INSERT INTO scores (player_id, as_of, season_score, recent_score, matchup_score, statcast_score, park_score, weather_score, bullpen_score, total_score)
-                VALUES (:player_id, :as_of, :s_score, :r_score, :m_score, :stat_score, :p_score, :w_score, :b_score, :t_score)
-            ''')
-            conn.execute(stmt, {
-                'player_id': player_id,
-                'as_of': as_of,
-                's_score': s_score,
-                'r_score': r_score,
-                'm_score': m_score,
-                'stat_score': stat_score,
-                'p_score': p_score,
-                'w_score': w_score,
-                'b_score': b_score,
-                't_score': t_score,
-            })
-        conn.commit()
+            conn.execute(sqlalchemy.text(
+                "INSERT INTO scores (player_id, as_of, season_score, recent_score, matchup_score, statcast_score, park_score, weather_score, bullpen_score, total_score) "
+                "VALUES (:p, :d, :s, :r, :m, :st, :pk, :w, :b, :t)"
+            ), {'p': player_id, 'd': as_of, 's': s_score, 'r': r_score, 'm': m_score, 'st': stat_score, 'pk': p_score, 'w': w_score, 'b': b_score, 't': t_score})
     
     logging.info('Scoring complete!')
 
